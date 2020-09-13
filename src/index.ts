9cfb8e144eca4b918bd08fbebc4cbb02b69d45bf
SkyLightQP <combbm@naver.com>
2020-09-13 16:34:23 +0900

    feat: add log4js code
    - chore: add log4js dependency

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import log4js from 'log4js';
+
+export const logger = log4js.getLogger();
+
+dotenv.config();
+
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'console'
+    },
+    default: {
+      type: 'file',
+      filename: 'logs/matwaetle.log',
+      pattern: '-yyyy-MM-dd',
+      compress: true
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['default', 'console'],
+      level: 'DEBUG'
+    }
+  }
+});
+logger.level = 'ALL';
