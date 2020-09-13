13f7f01d1889b0f152b051bdbcacf90c786ad498
SkyLightQP <combbm@naver.com>
2020-09-13 22:41:50 +0900

    feat: implement ProblemCommand.ts

--- src/commands/ProblemCommand.ts
+++ src/commands/ProblemCommand.ts
@@ -1,6 +1,8 @@
 import Discord, { Message, TextChannel } from 'discord.js';
+import dateFormat from 'dateformat';
 import CommandExecutor from '../interfaces/CommandExecutor';
 import config from '../config';
+import getBaekjoonData, { Tag } from '../services/solved';
 
 class ProblemCommand implements CommandExecutor {
   alias: string[] = ['출제', '출'];
@@ -20,19 +22,25 @@ class ProblemCommand implements CommandExecutor {
       return;
     }
 
-    const template = (n: string) => (
+    const date = dateFormat(new Date(), 'mm월 dd일');
+
+    const template = (n: string, tags: Tag[]) => (
       `
       https://www.acmicpc.net/problem/${n}
-      분류: ||{분류}||
+      분류: ||${tags.join(', ')}||
       `.trim()
     );
 
     const embed = new Discord.MessageEmbed()
-      .setTitle('asd')
+      .setTitle(date)
       .setColor('#009874');
 
-    args.forEach((text) => {
-      embed.addField(`#${text} {NAME}`, template(text), false);
+    const promises = args.map((n) => getBaekjoonData(n));
+    const result = await Promise.all(promises);
+
+    result.forEach((item) => {
+      if (item === undefined) return;
+      embed.addField(`#${item.id} ${item.title}`, template(item.id, item.tags), false);
     });
 
     await chan.send(embed);
